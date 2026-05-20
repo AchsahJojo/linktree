7d43b66d452ba870a31ca78d5e7e584b823b4181
Achsah Jojo <ajojo@csumb.edu>
2026-05-20 16:40:06 -0700

    Initial React project

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="page">
+      <section className="profile-card">
+        <section className="profile">
+          <div className="avatar" aria-label="Achsah Jojo profile picture">
+            <span>AJ</span>
+            <img src="/profile_pic.jpg" alt="Achsah Jojo" />
+          </div>
+
+          <h1>Achsah Jojo</h1>
+
+          <p className="profile-title">Software Developer · BizzNEST</p>
+
+          <p className="profile-location">📍 Santa Cruz, CA</p>
+        </section>
+
+        <p className="bio">Hello I love being a girly girl in tech.</p>
+        <div className="links">
+          <a
+            href="https://github.com/AchsahJojo"
+            target="_blank" // takes user to a new browser tab when clicked
+            rel="noreferrer" // adds security and privacy when opening a new tab
+          >
+            GitHub
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/achsah-jojo/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedin
+          </a>
+
+          <a
+            href="https://ajojo7.wixsite.com/achsahjojo"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Portfolio
+          </a>
+
+          <a
+            href="https://drive.google.com/file/d/13Oz3rAJTLj_Dr6WVHLTMvSrLLXO14Glz/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Resume
+          </a>
+        </div>
+
+        <footer className="site-footer">
+          <p>Made by Achsah Jojo for Senior Associate Technical Assessment</p>
+        </footer>
+      </section>
+    </main>
+  );
+}
+
+export default App;
